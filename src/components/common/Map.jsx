a3f99f28d73644f10cb1231e1f0b8a0a0622673c
Abdallah-704 <abdallahdaas18@gmail.com>
2025-04-29 17:33:40 +0300

    upload

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -108,13 +108,7 @@ const Map = ({ properties = [] }) => {
           );
         })}
       </MapContainer>
-      <button
-        onClick={toggleFullScreen}
-        className="absolute top-4 right-4 bg-indigo-600 text-white px-4 py-2 rounded-md shadow-md hover:bg-indigo-700 transition"
-        aria-label={isFullScreen ? "تصغير الخريطة" : "تكبير الخريطة"}
-      >
-        {isFullScreen ? "تصغير" : "تكبير"}
-      </button>
+      
     </div>
   );
 };
