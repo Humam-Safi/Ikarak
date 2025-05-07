8b5f1f2d78c4492ef80a98600442a331fd240c20
HumamSafi <abobaraasf@gmail.com>
2025-05-07 10:14:06 +0300

    uplaod

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -85,10 +85,17 @@ const Navbar = () => {
               الرئيسية
             </Link>
             <Link
-              to="/properties"
-              className="block text-gray-700 hover:text-white hover:bg-sky-900 px-3 py-2 rounded-md text-base font-semibold transition-colors duration-200"
+              to="/sale-properties"
+              className="block text-gray-700 hover:text-white hover:bg-sky-900 px-3 py-2 rounded-md text-sm font-semibold transition-colors duration-200"
             >
-              العقارات
+              عروض البيع
+            </Link>
+
+            <Link
+              to="/rent-properties"
+              className="block text-gray-700 hover:text-white hover:bg-sky-900 px-3 py-2 rounded-md text-sm font-semibold transition-colors duration-200"
+            >
+              عروض الأجار
             </Link>
 
             <Link
